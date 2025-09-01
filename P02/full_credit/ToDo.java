fddc7ce3fc731b65a5da5bc27425d01118c092ba
Alka Shukla <shuklaalka623@gmail.com>
2025-09-01 03:48:06 -0500

    Getting number of items from user or setting default to 5.

---
+++ P02/full_credit/ToDo.java
@@ -0,0 +1,14 @@
+package P02.full_credit;
+
+import java.util.Scanner;
+public class ToDo {
+    public static void main(String[] args){
+    int numItems=5;
+    if(args.length>0){
+    numItems=Integer.parseInt(args[0]);
+    
+    } 
+    }
+
+  
+}
