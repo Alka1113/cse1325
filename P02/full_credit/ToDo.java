95f00eb103b82e4dc6e259da7ca8954ea52741d8
Alka Shukla <shuklaalka623@gmail.com>
2025-09-01 20:28:07 -0500

    Correcting indirect reference to obejct of array

--- P02/full_credit/ToDo.java
+++ P02/full_credit/ToDo.java
@@ -16,25 +16,25 @@ public class ToDo {
     Scanner sc=new Scanner(System.in);
      Item list[]=new Item[numItems];
      for(int i=0; i<numItems; i++){
-        System.out.println("Task no." + i);
+       System.out.println("Task no." + i);
        String task=sc.nextLine();
        System.out.println("Priority order");
        int priority=sc.nextInt();
-    //    Item tmpItem=new Item();
-    //    tmpItem.task=task;
-    //    tmpItem.priority=priority;
-       list[i].task=task;
-       list[i].priority=priority;
+       sc.nextLine();
+       Item tmpItem=new Item();
+       tmpItem.task=task;
+       tmpItem.priority=priority;
+       list[i] = tmpItem;
+    //    list[i].task=task;
+    //    list[i].priority=priority;
      }
      System.out.println("Printing the list");
      for(int priority=1; priority<6; priority++){
-        for(int i=0; i<list.length; i++){
+        for(int i=0; i<list.length; i++){   
             if(list[i].priority==priority){
                 System.out.println(list[i].task);
             }
         }
      }
-    }
-  
-  
+    }  
 }
