f22a9231ae283ead2f863fec544914a83a662902
Alka Shukla <shuklaalka623@gmail.com>
2025-09-01 19:12:08 -0500

    Loop to get user input for each item

--- P02/full_credit/ToDo.java
+++ P02/full_credit/ToDo.java
@@ -6,9 +6,22 @@ public class ToDo {
     int numItems=5;
     if(args.length>0){
     numItems=Integer.parseInt(args[0]);
-    
     } 
-    }
+    Scanner sc=new Scanner(System.in);
+     Item[] list=new Item[numItems];
+     for(int i=0; i<numItems; i++){
+        System.out.println("Task no." + i);
+       String task=sc.nextLine();
+       System.out.println("Priority order");
+       int priority=sc.nextInt();
+    //    Item tmpItem=new Item();
+    //    tmpItem.task=task;
+    //    tmpItem.priority=priority;
+       list[i].task=task;
+       list[i].priority=priority;
 
+     }
+    }
+  
   
 }
