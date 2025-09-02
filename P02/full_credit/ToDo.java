cc6060461cfce35eaaf13b8280367c07a0350500
Alka Shukla <shuklaalka623@gmail.com>
2025-09-01 19:50:11 -0500

    Final submission

--- P02/full_credit/ToDo.java
+++ P02/full_credit/ToDo.java
@@ -1,14 +1,20 @@
 package P02.full_credit;
 
 import java.util.Scanner;
+class Item{
+    String task;
+    int priority;
+}
+
 public class ToDo {
+
     public static void main(String[] args){
     int numItems=5;
     if(args.length>0){
     numItems=Integer.parseInt(args[0]);
     } 
     Scanner sc=new Scanner(System.in);
-     Item[] list=new Item[numItems];
+     Item list[]=new Item[numItems];
      for(int i=0; i<numItems; i++){
         System.out.println("Task no." + i);
        String task=sc.nextLine();
@@ -19,7 +25,14 @@ public class ToDo {
     //    tmpItem.priority=priority;
        list[i].task=task;
        list[i].priority=priority;
-
+     }
+     System.out.println("Printing the list");
+     for(int priority=1; priority<6; priority++){
+        for(int i=0; i<list.length; i++){
+            if(list[i].priority==priority){
+                System.out.println(list[i].task);
+            }
+        }
      }
     }
   
